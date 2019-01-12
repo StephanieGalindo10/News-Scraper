e468aebfd0e4f0a03afcf12b3a2c054bc69700c9
stephaniegalindo10 <stephanimatedworld@gmail.com>
2019-01-11 20:57:37 -0800

    added links to export

--- server.js
+++ server.js
@@ -11,3 +11,64 @@ var request = require("request");
 // =============================================================
 const PORT = process.env.PORT || 3000;
 let app = express();
+
+app
+    .use(bodyParser.json())
+    .use(bodyParser.urlencoded({ extended:true }))
+    .use(bodyParser.text())
+    .use(bodyParser.json({ type: 'application/vnd.api+json' }))
+    .use(methodOverride('_method'))
+    .use(logger('dev'))
+    .use(express.static(__dirname + '/public'))
+    .engine('handlebars', exphbs({ defaultLayout: 'main' }))
+    .set('view engine', 'handlebars')
+    .use(require('./controllers'));
+
+
+    // Mongoose///
+
+var Note = require("./models/Note");
+var Article = require("./models/Article");
+var databaseUrl = 'mongodb://localhost/scrap';
+
+if (process.env.MONGODB_URI) {
+	mongoose.connect(process.env.MONGODB_URI);
+}
+else {
+	mongoose.connect(databaseUrl);
+};
+
+mongoose.Promise = Promise;
+var db = mongoose.connection;
+
+db.on("error", function(error) {
+	console.log("Mongoose Error: ", error);
+});
+
+db.once("open", function() {
+	console.log("Mongoose connection successful.");
+});
+
+
+//set engine and default for handlebars
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var router = express.Router();
+
+// Require routes file pass router object
+require("./config/routes")(router);
+
+// Have every request go through router middlewar
+app.use(router);
+
+//set port
+var port = process.env.PORT || 3001;
+
+//setup listener
+app.listen(port, function() {
+  console.log("app running on port " + port);
+});
+
+module.exports = app;
