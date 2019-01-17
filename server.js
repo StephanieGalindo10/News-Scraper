014d558a759f6b7d7da23f2824c6c769712af570
stephaniegalindo10 <stephanimatedworld@gmail.com>
2019-01-17 01:20:53 -0800

    change server

--- server.js
+++ server.js
@@ -44,6 +44,9 @@ var routes = require('./controllers/controllers');
 app.use('/', routes);
 
 var port = process.env.PORT || 3000;
+
+var port = process.env.PORT || 3000;
 app.listen(port, function(){
   console.log('Listening on PORT ' + port);
 });
+
